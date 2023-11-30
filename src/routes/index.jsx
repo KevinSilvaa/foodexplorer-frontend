fa1ab307e2a23177fa14268529f6092307bd091d
Kevin <kevinsilvarj10@gmail.com>
2023-11-30 08:33:02 -0300

    Validar usuário via token

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,10 +6,21 @@ import { USER_ROLE } from "../utils/roles";
 import { AuthRoutes } from "./auth.routes";
 import { AdminRoutes } from "./admin.routes";
 import { CustomerRoutes } from "./customer.routes";
+import { useEffect } from "react";
+import { api } from "../services/api";
 
 export function Routes() {
 
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get("/users/validated")
+    .catch(error => {
+      if (error.response?.status === 401) {
+        signOut();
+      }
+    })
+  }, []);
 
   function AcessRoute() {
     switch(user.role) {
