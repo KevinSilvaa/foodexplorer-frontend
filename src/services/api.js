28173233594e74fd5ff18f83cf53af40d74d1e29
Kevin <kevinsilvarj10@gmail.com>
2023-11-30 06:52:51 -0300

    Adicionar url do deploy do Back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://foodexplorer-backend-8v1b.onrender.com",
   withCredentials: true
 });
